53da7c89933a64126136b17e49059c1872993adf
yupno <83811751+Yupno25@users.noreply.github.com>
2024-08-02 01:24:08 +0200

    Added first item (example item).

---
+++ src/main/java/com/yupno/culinary_wizardry/item/ModItems.java
@@ -0,0 +1,22 @@
+package com.yupno.culinary_wizardry.item;
+
+import com.yupno.culinary_wizardry.CulinaryWizardryMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, CulinaryWizardryMod.MOD_ID);
+
+    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
